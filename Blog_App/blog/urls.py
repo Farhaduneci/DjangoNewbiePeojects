9c1799706145dbb96c483d7dc157a54c741b2674
Farhad <farhaduneci@gmail.com>
2022-07-13 00:24:37 +0430

    ADD: BlogApp, Post model, CRUD

--- Blog_App/blog/urls.py
+++ Blog_App/blog/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("posts/new/", BlogCreateView.as_view(), name="new_post"),
+    path("posts/<int:pk>/edit/", BlogUpdateView.as_view(), name="edit_post"),
+    path("posts/<int:pk>/delete/", BlogDeleteView.as_view(), name="delete_post"),
 ]
--- Blog_App/blog/views.py
+++ Blog_App/blog/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
 
 from .models import Post
 
@@ -11,3 +13,23 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "blog/new_post.html"
+    fields = ["author", "title", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "blog/edit_post.html"
+    fields = ["author", "title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "blog/delete_post.html"
+
+    def get_success_url(self):
+        return reverse("home")
